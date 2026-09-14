421615f0831ba80ed3fd23ed17bf969d834ee050
lilja <lilja.tatti@hotmail.com>
2026-09-14 15:56:13 +0300

    tumma tila hienosäätö

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 const button = document.getElementById("tummaTila");
 
+function updateButton() {
+    if (document.body.classList.contains("dark-mode")) {
+        button.textContent = "☀️ Vaalea tila";
+    } else {
+        button.textContent = "🌙 Tumma tila";
+    }
+}
+
 button.addEventListener("click", function() {
     document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("darkMode", "on");
+    } else {
+        localStorage.setItem("darkMode", "off");
+    }
+
+    updateButton();
 });
+
+if (localStorage.getItem("darkMode") === "on") {
+    document.body.classList.add("dark-mode");
+}
+
+updateButton();
